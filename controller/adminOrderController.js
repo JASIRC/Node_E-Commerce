6f6c68e688ca469d873a102147c7feff4c3dc23c
JASIRC <jasirc10@gmail.com>
2024-06-20 21:22:03 +0530

    updated

--- controller/adminOrderController.js
+++ controller/adminOrderController.js
@@ -15,27 +15,29 @@ export const adminOrder= async(req,res,next)=>{
 
 
 export const revenue = async (req,res,next)=>{
-    try{
-        const total=await Orders.aggregate([
-            {
-                $addFields:{
-                    productId:{$ifNull:["$productId",[]]}
-                }
-            },
-            {
-                $group:{
-                    _id:null,
-                    totalProduct:{$sum:{$size:"$productId"}},
-                    totalRevenue:{$sum:"$totalPrice"}
-                }
+    try {
+        const total = await Orders.aggregate([
+          {
+            $unwind: {
+              path: "$productId",
+              preserveNullAndEmptyArrays: true
             }
+          },
+          {
+            $group: {
+              _id: null,
+              totalProduct: { $sum: "$totalItem" },
+              totalRevenue: { $sum: "$totalPrice" }
+            }
+          }
         ]);
-        if(total.length>0){
-            return res.status(200).json({message:"Success", Data:total[0]})
-        }else{
-            return res.status(200).json({message:"Success", Data:{totalProduct:0, totalRevenue:0}});    
+    
+        if (total.length > 0) {
+          return res.status(200).json({ message: "Success", Data: total[0] });
+        } else {
+          return res.status(200).json({ message: "Success", Data: { totalProduct: 0, totalRevenue: 0 } });
         }
-    }catch(error){
-        return next(error)
-    }
+      } catch (error) {
+        return next(error);
+      }
 };
